8ac11b09c5375a15a3d14bb461d651a52fea145f
dixitpriyanshu <priyanshu.dixit.2910@gmail.com>
2025-02-19 00:30:50 +0530

    WIP

--- app/(index)/index.tsx
+++ app/(index)/index.tsx
@@ -1,9 +1,7 @@
-import { ThemedText } from "@/components/ThemedText";
 import { BodyScrollView } from "@/components/ui/body-scroll-view";
 import Button from "@/components/ui/button";
 import { useClerk } from "@clerk/clerk-expo";
 import { router } from "expo-router";
-import { View } from "react-native";
 
 export default function HomeScreen() {
   const { signOut } = useClerk();
